21172a0f487b2c92785d356cab9b9ead6f9e600d
Ariel Souza <arielsvadev@gmail.com>
2026-04-22 20:41:09 -0300

    feat(api): add swagger with Swashbuckle to development environment

--- src/WebIdentity.Api/Program.cs
+++ src/WebIdentity.Api/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddIdentityApiEndpoints<IdentityUser>()
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -28,6 +29,8 @@ app.MapIdentityApi<IdentityUser>();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
